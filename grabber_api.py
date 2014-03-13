c24bf305c317b6b27a106dacfd19a500e3fe78e8
Stefan Hesse <stefan.hesse@tu-dortmund.de>
2014-03-13 23:21:06 +0100

    updates

--- grabber_api.py
+++ grabber_api.py
@@ -28,8 +28,8 @@ class GrabberApi(object):
 
     def getVideoUrl(self):
         self._parse()
-        
-        return self.parseResults['url'].replace("https://", "http://")
+
+        return self.parseResults['url']
 
     def getVideoTitle(self):
         self._parse()
